1f8758f5371db8e2404012b3d114dff5829ac755
Ludovic Claude <ludovic.claude54@gmail.com>
2018-02-16 11:08:49 +0100

    Moving packages to ch.chuv.lren.woken

--- src/main/scala/ch/chuv/lren/woken/messages/query/json_protocol.scala
+++ src/main/scala/ch/chuv/lren/woken/messages/query/json_protocol.scala
@@ -1,27 +1,28 @@
 /*
- * Copyright 2017 Human Brain Project MIP by LREN CHUV
+ * Copyright (C) 2017  LREN CHUV for Human Brain Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package ch.chuv.lren.woken.messages.query
 
 import java.time.{ LocalDateTime, OffsetDateTime, ZoneOffset }
 
-import eu.hbp.mip.woken.messages.datasets.DatasetsProtocol
-import eu.hbp.mip.woken.messages.query.filters.QueryFiltersProtocol
-import eu.hbp.mip.woken.messages.variables.VariablesProtocol
-import eu.hbp.mip.woken.utils.JsonEnums
+import ch.chuv.lren.woken.messages.datasets.DatasetsProtocol
+import ch.chuv.lren.woken.messages.query.filters.QueryFiltersProtocol
+import ch.chuv.lren.woken.messages.variables.VariablesProtocol
+import ch.chuv.lren.woken.utils.JsonEnums
 import spray.json._
 
 trait QueryProtocol extends DefaultJsonProtocol with JsonEnums {
